0b1e2017bf450da32d6b7997c6cbdeaf2e55c49e
vossso <sophievoss@gmx.de>
2021-06-02 20:39:48 +0200

    test 2 google-analytics

--- src/hooks/useCookiePolicy.tsx
+++ src/hooks/useCookiePolicy.tsx
@@ -3,17 +3,20 @@ import { useCookies } from "react-cookie";
 export const pushToDataLayer = (
   obj: Record<string, any> | Record<string, any>[]
 ) => {
-  if (
-    typeof window !== "undefined" &&
-    typeof window["dataLayer"] !== "undefined"
-  ) {
-    const dataLayer = window["dataLayer"];
-    obj && (obj as Record<string, any>[]).length
-      ? (obj as Record<string, any>[]).forEach((value) => dataLayer.push(value))
-      : dataLayer.push(obj);
-  }
+  // if (
+  //   typeof window !== "undefined" &&
+  //   typeof window["dataLayer"] !== "undefined"
+  // ) {
+  //   console.log("object", obj)
+  //   const dataLayer = window["dataLayer"];
+  //   obj && (obj as Record<string, any>[]).length
+  //     ? (obj as Record<string, any>[]).forEach((value) => dataLayer.push(value))
+  //     : dataLayer.push(obj);
+  // }
+  const dataLayer = window["dataLayer"] || [];
+  console.log(dataLayer.push(obj));
+  dataLayer.push(obj);
 };
-
 
 const useCookiePolicy = () => {
   const [, setCookie] = useCookies();
@@ -21,9 +24,9 @@ const useCookiePolicy = () => {
   const setCookiePolicy = (value: number) => {
     pushToDataLayer([{ marketingCookieConsent: value === 1 }]);
     if (value === 1) {
-      pushToDataLayer({"js" : new Date()});
-      pushToDataLayer({"config":  "G-289WBD66JY"});
-      pushToDataLayer({ event: "consentChange" });
+      pushToDataLayer({ js: new Date() });
+      pushToDataLayer({ config: "G-289WBD66JY" });
+      // pushToDataLayer({ event: "consentChange" });
     }
     setCookie("cookie-policy", value);
   };
