1ac5cc9ddfdfbd4bce064a181663980937474100
vossso <sophievoss@gmx.de>
2021-06-02 21:00:30 +0200

    test 3 google-analytics

--- src/hooks/useCookiePolicy.tsx
+++ src/hooks/useCookiePolicy.tsx
@@ -1,32 +1,13 @@
 import { useCookies } from "react-cookie";
-
-export const pushToDataLayer = (
-  obj: Record<string, any> | Record<string, any>[]
-) => {
-  // if (
-  //   typeof window !== "undefined" &&
-  //   typeof window["dataLayer"] !== "undefined"
-  // ) {
-  //   console.log("object", obj)
-  //   const dataLayer = window["dataLayer"];
-  //   obj && (obj as Record<string, any>[]).length
-  //     ? (obj as Record<string, any>[]).forEach((value) => dataLayer.push(value))
-  //     : dataLayer.push(obj);
-  // }
-  const dataLayer = window["dataLayer"] || [];
-  console.log(dataLayer.push(obj));
-  dataLayer.push(obj);
-};
+import ReactGA from "react-ga";
 
 const useCookiePolicy = () => {
   const [, setCookie] = useCookies();
 
   const setCookiePolicy = (value: number) => {
-    pushToDataLayer([{ marketingCookieConsent: value === 1 }]);
     if (value === 1) {
-      pushToDataLayer({ js: new Date() });
-      pushToDataLayer({ config: "G-289WBD66JY" });
-      // pushToDataLayer({ event: "consentChange" });
+      ReactGA.initialize("G-289WBD66JY");
+      ReactGA.pageview(window.location.pathname + window.location.search);
     }
     setCookie("cookie-policy", value);
   };
