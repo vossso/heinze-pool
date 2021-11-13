90c2372007c531b47206295014b6007ee449abba
vossso <sophievoss@gmx.de>
2021-11-13 18:22:11 +0100

    quick-fix(cms): remove preview

--- gatsby-config.js
+++ gatsby-config.js
@@ -69,9 +69,9 @@ module.exports = {
     },
     {
       resolve: "gatsby-plugin-netlify-cms",
-      options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
-      },
+      // options: {
+      //   modulePath: `${__dirname}/src/cms/cms.js`,
+      // },
     },
     {
       resolve: "gatsby-plugin-purgecss", // purges all unused/unreferenced css rules
